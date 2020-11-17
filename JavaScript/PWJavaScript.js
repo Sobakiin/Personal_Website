9669850eb8cc9eacd41326712e857c7ac8471ecc
Sobakiin <wgreen16@my.bcit.ca>
2020-11-17 15:20:55 -0800

    I added a prompt and a loop that re-prompts the user for a course code. It re-prompts the user if it is an invalid course code.

--- JavaScript/PWJavaScript.js
+++ JavaScript/PWJavaScript.js
@@ -1,1 +1,6 @@
 const courseList = [{code:"ACIT 1620",name:"Fudamental Web Technologies"}, {code:"MATH 1310", name:"Technical Math for IT"},{code:"COMM1116", name:"Business Communications"}]
+
+var in_code = prompt("Please enter a 4-digit course code:", "0000");
+while(isNaN(in_code) || length(in_code)!=4 ){
+    let in_code = prompt("That is not a valid input, please enter a 4-digit course code:", "0000")
+}
