be055543b6e9dc2662d94f0536024ed8c8a59b12
Sobakiin <wgreen16@my.bcit.ca>
2020-11-17 16:28:41 -0800

    I have fixed as many errors as possible in my script, and I have made the script run after the page has lodade

--- JavaScript/PWJavaScript.js
+++ JavaScript/PWJavaScript.js
@@ -1,18 +1,18 @@
 const courseList = [{code:"ACIT 1620",name:"Fudamental Web Technologies"}, {code:"MATH 1310", name:"Technical Math for IT"},{code:"COMM1116", name:"Business Communications"}]
 
 var in_code = prompt("Please enter a 4-digit course code:", "0000");
-while(isNaN(in_code) || length(in_code)!=4 ){
+while(isNaN(in_code) || in_code.length !=4 ){
     let in_code = prompt("That is not a valid input, please enter a 4-digit course code:", "0000")
 }
 
 var exist_check = 0; 
-for (i = 0; i<length(courseList);i++){
-    if (in_code in courseList[i].code){
+for ( i = 0; i<=courseList.length; i++){
+    if (courseList[i].code.includes(in_code)){
         exist_check++;
-        console.log("Yes, I am taking the course:",courseList[i].code)
-    }
+        console.log("Yes, I am taking the course:",  courseList[i].code);
+    };
 }
 if(exist_check == 0){
-    courseList.push({code:in_code, name: null})
-    console.log("A success message.")
+    courseList.push({code:in_code, name: null});
+    console.log("A success message.");
 }
