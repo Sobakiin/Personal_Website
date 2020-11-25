95fd5ff33c52bc0a17efc5e7eccd837f98a64283
Sobakiin <wgreen16@my.bcit.ca>
2020-11-24 22:09:14 -0800

    Began findCourse function, can see if course already exists.

--- JavaScript/PWJavaScript.js
+++ JavaScript/PWJavaScript.js
@@ -1,4 +1,3 @@
-// const courseList = [{code:"ACIT 1620",name:"Fudamental Web Technologies"}, {code:"MATH 1310", name:"Technical Math for IT"},{code:"COMM1116", name:"Business Communications"}]
 
 function createCourseArray(){
 
@@ -25,19 +24,25 @@ function createCourseArray(){
 
 let courses_array = createCourseArray()
 
-// var in_code = prompt("Please enter a 4-digit course code:", "0000");
-// while(isNaN(in_code) || in_code.length !=4 ){
-//     let in_code = prompt("That is not a valid input, please enter a 4-digit course code:", "0000")
-// }
 
-// var exist_check = 0; 
-// for ( i = 0; i<=courseList.length; i++){
-//     if (courseList[i].code.includes(in_code)){
-//         exist_check++;
-//         console.log("Yes, I am taking the course:",  courseList[i].code);
-//     };
-// }
-// if(exist_check == 0){
-//     courseList.push({code:in_code, name: null});
-//     console.log("A success message.");
-// }
+function findCourse(courseList){
+
+
+    var in_code = prompt("Please enter a 4-digit course code:", "0000");
+    while(isNaN(in_code) || in_code.length !=4 ){
+        let in_code = prompt("That is not a valid input, please enter a 4-digit course code:", "0000")
+    }
+
+var exist_check = 0; 
+for ( i = 0; i<=courseList.length; i++){
+    if (courseList[i].code.includes(in_code)){
+        exist_check++;
+
+        for(let item of document.querySelectorAll(".grid-item1")){
+            if(item.textContent.includes(in_code)) {item.style.backgroundcColor = "green"}
+        }
+        
+    };
+}
+
+}
